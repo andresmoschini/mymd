0877fa1c7b94baa03a3a33eb7d2115e3f8c17079
Andres Moschini <andresmoschini@gmail.com>
2012-03-06 08:58:54 -0300

    My first commit

---
+++ Scripts/mymd.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="marked.js" />
+/// <reference path="jquery-1.7.1.js" />
+
+//TODO: 
+//  Permitir cambiar entre distintos markdowns
+
+//Ver: 
+//  http://github.github.com/github-flavored-markdown/
+//  https://github.com/chjj/marked (Syntax Highlighting)
+//  http://softwaremaniacs.org/playground/showdown-highlight/
+//  http://github.github.com/github-flavored-markdown/preview.html
+//  http://www.toptensoftware.com/markdowndeep/
+
+
+$(function () {
+    var $input = $("#inputPane");
+    var $preview = $("#previewPane");
+
+    marked.setOptions({
+        gfm: true,
+        pedantic: false,
+        sanitize: false
+    });
+
+    $input.keyup(function () {
+        $preview.html(marked.parse($input.val()));
+    });
+});
